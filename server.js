5199b734589fb7d02610b6be198faa6a58c5e3a7
Dustin McQuay <dustin-mcquay@pluralsight.com>
2020-05-27 12:42:43 -0600

    test update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    res.send('hello world')
+    res.send('hello world 2')
 })
 
 app.listen(PORT, () => {
